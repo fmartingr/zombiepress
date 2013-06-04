bff93a8f9809843716a984bfe8d94b4db31c247d
Felipe Martín <fmartin@bytepix.com>
2013-06-04 16:43:15 +0200

    Added blog base templates

--- zombiepress/apps/blog/views.py
+++ zombiepress/apps/blog/views.py
@@ -1,5 +1,10 @@
-def list(request):
-    pass
+#from zombiepress.utils import render
+from django.shortcuts import render_to_response
 
-def entry(request):
-    pass
+
+def list(request):
+    return render_to_response('blog/list.html')
+
+
+def entry(request, year, month, day, slug):
+    return render_to_response('blog/entry.html')
