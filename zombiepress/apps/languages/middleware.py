c3723b5f9e462b1303b760e48d368e75c4c85d50
Felipe Martín <fmartingr@me.com>
2013-06-07 13:42:42 +0200

    Added a redirect to LanguageURLMiddleware if the locale isn't found

--- zombiepress/apps/languages/middleware.py
+++ zombiepress/apps/languages/middleware.py
@@ -1,15 +1,20 @@
-import django.core.exceptions
-from django.conf import settings
 from django.utils import translation
+from django.utils.encoding import iri_to_uri
+from django.http import HttpResponseRedirect
 from zombiepress.apps.languages.utils import strip_language_code_from_url
 
 
 class LanguageURLMiddleware(object):
-    def __init__(self):
-        if not settings.MULTILANGUAGE:
-            raise django.core.exceptions.MiddlewareNotUsed()
-
     def process_request(self, request):
-        locale, path = strip_language_code_from_url(request.path_info)
-        translation.activate(locale)
+        locale, path, found = strip_language_code_from_url(request.path_info)
+
+        # If locale is not found, we redirect to the returned locale
+        # which should be the default one.
+        if not found:
+            return HttpResponseRedirect(iri_to_uri("/%s%s" % (locale, path)))
+
+        # If a locale is found, we set the path without it and continue
+        # as normal.
         request.path_info = path
+        translation.activate(locale)
+        request.LANGUAGE_CODE = translation.get_language()
--- zombiepress/apps/languages/utils.py
+++ zombiepress/apps/languages/utils.py
@@ -3,14 +3,16 @@ from zombiepress.apps.languages.models import Language
 
 
 def strip_language_code_from_url(path):
+    found = False
     locale = get_default_locale()
     locale_check = languages_settings.PATH_LOCALE_REGEX.match(path)
     if locale_check:
         custom_locale = locale_check.group('locale')
         if check_if_locale_is_available(custom_locale):
             locale = custom_locale
+            found = True
         path = path[3:]
-    return locale, path
+    return locale, path, found
 
 
 def check_if_locale_is_available(locale):
