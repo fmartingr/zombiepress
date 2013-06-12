cdecaa01566ddc8c20d33cbc971c838eee1ffd53
Felipe Martín <fmartingr@me.com>
2013-06-12 14:43:44 +0200

    Added datetime helper to blog app

---
+++ zombiepress/apps/blog/helpers.py
@@ -0,0 +1,14 @@
+from jingo import register
+from django.utils.translation import ugettext as _
+from django.utils.encoding import smart_unicode
+from django.conf import settings
+from pytz import timezone
+
+
+@register.filter
+def dt(t, fmt=None):
+    """Call ``datetime.strftime`` with the given format string."""
+    tz = timezone(settings.TIME_ZONE)
+    if fmt is None:
+        fmt = _('%B %e, %Y')
+    return smart_unicode(tz.normalize(t).strftime(fmt)) if t else u''
